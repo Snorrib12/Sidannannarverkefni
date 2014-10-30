b4aeb56e83b557cbff398f3eb7faff0644f86795
Snorri <Snorrus@gmail.com>
2014-10-30 17:16:30 +0000

    player class and playertest class finished

--- player.java
+++ player.java
@@ -15,4 +15,13 @@ public class player {
 	public void setSymbol(String newSymbol){
 		this.symbol = newSymbol;
 	}
+	public void win(){
+		this.addScore();
+		if(this.symbol == "x") setSymbol("o");
+		else setSymbol("x");
+	}
+	public void lose(){
+		if(this.symbol == "x") setSymbol("o");
+		else setSymbol("x");
+	}
 }
--- playertest.java
+++ playertest.java
@@ -9,8 +9,9 @@ public class playertest {
 	@Test
 	public void setsymboltest() {
 		player testplayer = new player("x");
-		testplayer.setSymbol("y");
-		assertTrue("symbol was changed", "y" == testplayer.symbol);
+		assertTrue("symbol is x", "x" == testplayer.symbol);
+		testplayer.setSymbol("o");
+		assertTrue("symbol was changed", "o" == testplayer.symbol);
 	}
 	@Test
 	public void addScoretest(){
@@ -20,6 +21,24 @@ public class playertest {
 		assertEquals("score is now 1", testplayer.score , 1);
 		testplayer.addScore();
 		assertEquals("score is now 2", testplayer.score ,2);
+	}
+	@Test
+	public void winTest(){
+		player testplayer = new player("x");
+		assertEquals("score is 0",testplayer.score ,0);
+		assertEquals("symbol is x",testplayer.symbol, "x");
+		testplayer.win();
+		assertEquals("score is 1",testplayer.score ,1);
+		assertEquals("symbol is y",testplayer.symbol, "o");
+	}
+	@Test
+	public void loseTest(){
+		player testplayer = new player("x");
+		assertEquals("score is 0",testplayer.score ,0);
+		assertEquals("symbol is x",testplayer.symbol, "x");
+		testplayer.lose();
+		assertEquals("score is 1",testplayer.score ,0);
+		assertEquals("symbol is y",testplayer.symbol, "o");
 	}
 
 }
